cc91025c2ee1514922e33e9e9d9051fcc61e0bba
isofinly <isofinly@protonmail.com>
2024-03-17 01:16:47 +0300

    *&

--- src/frontend/app/lab3/page.tsx
+++ src/frontend/app/lab3/page.tsx
@@ -298,7 +298,7 @@ const LinearEquationPage = () => {
               <div className="py-2">
                 <input
                   id="mtrx"
-                  value={solution ? solution.calculated_integral : ""}
+                  value={solution ? solution.integral_value : ""}
                   disabled
                   className="px-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
